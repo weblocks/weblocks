e5fbefb359f4b6ffc0cb3c11a7ba0f5cfe726f10
weblockers <hiroyuki@weblocks.net>
2021-01-22 15:07:48 +0900

    Add password change script.

--- app/trans/languages/mysql.php
+++ app/trans/languages/mysql.php
@@ -2,6 +2,7 @@
 $options = [
     'content' => [
         'EXIST USER' => "select User from mysql.user where User = '{user}'",
+        'CHANGE PASSWORD' => "ALTER USER '{user}'@'localhost' identified BY '{password}'",
         'CREATE USER' => "create user '{user}'@'localhost' identified by '{password}'",
         'CREATE DATABASE' => "create database {database} character set = utf8",
         'GRANT ALL' => "grant all on {database}.* to '{user}'@'localhost'",
